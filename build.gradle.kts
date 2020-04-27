d29ea135666c532a21cd12192026a0da3743bb65
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-04-26 22:12:17 -0400

    build: don't include LogBack as runtime dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,12 +14,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("ch.qos.logback", "logback-classic", "1.2.3")
-
     implementation("org.postgresql", "postgresql", "42.1.4")
     implementation("org.jetbrains.exposed", "exposed-core", "0.23.1")
     implementation("org.jetbrains.exposed", "exposed-jdbc", "0.23.1")
 
+    testImplementation("ch.qos.logback", "logback-classic", "1.2.3")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.5.2")
 }
 
